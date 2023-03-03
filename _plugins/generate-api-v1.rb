cdbb83ad7d2716fab8f542bc6376c1c1e2c2b5a3
Marc Wrobel <marc.wrobel@gmail.com>
2023-03-03 13:54:14 +0100

    Improve schema

--- _plugins/generate-api-v1.rb
+++ _plugins/generate-api-v1.rb
@@ -63,13 +63,11 @@ module ApiV1
 
     def add_index_page(site)
       site_url = site.config['url']
-      site.pages << JsonPage.new(site, '/', 'index', {
-        result: [
-          { name: "products", uri: "#{site_url}/api/v#{ApiV1::MAJOR_VERSION}/products/" },
-          { name: "categories", uri: "#{site_url}/api/v#{ApiV1::MAJOR_VERSION}/categories/" },
-          { name: "tags", uri: "#{site_url}/api/v#{ApiV1::MAJOR_VERSION}/tags/" },
-        ]
-      })
+      site.pages << JsonPage.new(site, '/', 'index', [
+        { name: "products", uri: "#{site_url}/api/v#{ApiV1::MAJOR_VERSION}/products/" },
+        { name: "categories", uri: "#{site_url}/api/v#{ApiV1::MAJOR_VERSION}/categories/" },
+        { name: "tags", uri: "#{site_url}/api/v#{ApiV1::MAJOR_VERSION}/tags/" },
+      ])
     end
 
     def add_product_pages(site)
@@ -108,13 +106,13 @@ module ApiV1
     end
 
     def add_all_categories_page(site, products)
-      site.pages << JsonPage.new(site, '/categories/', 'index', {
-        result: products
-                  .map { |product| product.data['category'] }
-                  .uniq
-                  .sort
-                  .map { |category| { name: category, uri: "#{site.config['url']}/api/v#{ApiV1::MAJOR_VERSION}/categories/#{category}/" } }
-      })
+      all_categories = products.map { |product| product.data['category'] }.uniq.sort
+      site.pages << JsonPage.new(site, '/categories/', 'index',
+        all_categories.map { |category| {
+          name: category,
+          uri: "#{site.config['url']}/api/v#{ApiV1::MAJOR_VERSION}/categories/#{category}/"
+        }}
+      )
     end
 
     def add_tag_pages(site, products)
@@ -130,13 +128,13 @@ module ApiV1
     end
 
     def add_all_tags_page(site, products)
-      site.pages << JsonPage.new(site, '/tags/', 'index', {
-        result: products
-                  .flat_map { |product| product.data['tags'] }
-                  .uniq
-                  .sort
-                  .map { |tag| { name: tag, uri: "#{site.config['url']}/api/v#{ApiV1::MAJOR_VERSION}/tags/#{tag}/" } }
-      })
+      all_tags = products.flat_map { |product| product.data['tags'] }.uniq.sort
+      site.pages << JsonPage.new(site, '/tags/', 'index',
+        all_tags.map { |tag| {
+          name: tag,
+          uri: "#{site.config['url']}/api/v#{ApiV1::MAJOR_VERSION}/tags/#{tag}/"
+        }}
+      )
     end
 
     def add_to_map(map, key, page)
@@ -149,14 +147,15 @@ module ApiV1
   end
 
   class JsonPage < Jekyll::Page
-    def initialize(site, path, name, data)
+    def initialize(site, path, name, data, metadata = {})
       @site = site
       @base = site.source
       @dir = "api/v#{ApiV1::MAJOR_VERSION}#{path}"
       @name = "#{name}.json"
       @data = {}
       @data['layout'] = 'json'
-      @data['data'] = data
+      @data['data'] = metadata
+      @data['data']['result'] = data
       @data['data']['schema_version'] = ApiV1::VERSION
 
       self.process(@name)
@@ -165,20 +164,22 @@ module ApiV1
 
   class ProductsJsonPage < JsonPage
     def initialize(site, path, name, products)
-      super(site, path, name, {
-        total: products.size(),
-        products: products.map { |product| {
+      super(site, path, name,
+        products.map { |product| {
           name: product.data['id'],
           label: product.data['title'],
           category: product.data['category'],
           tags: product.data['tags'],
           identifiers: product.data['identifiers']
-          .map { |identifier| {
-            type: identifier.keys.first,
-            id: identifier.values.first
-          }},
+            .map { |identifier| {
+              type: identifier.keys.first,
+              id: identifier.values.first
+            }
+          },
           uri: "#{site.config['url']}/api/v#{ApiV1::MAJOR_VERSION}/products/#{product.data['id']}/",
-        }}
+        }
+      }, {
+        total: products.size()
       })
     end
   end
@@ -192,36 +193,39 @@ module ApiV1
         category: product.data['category'],
         tags: product.data['tags'],
         identifiers: product.data['identifiers']
-        .map { |identifier| {
-          type: identifier.keys.first,
-          id: identifier.values.first
-        }},
+          .map { |identifier| {
+            type: identifier.keys.first,
+            id: identifier.values.first
+          }
+        },
         links: {
           icon: product.data['iconUrl'],
           html: "#{site.config['url']}/#{id}",
           releasePolicyLink: product.data['releasePolicyLink'],
         },
         versionCommand: product.data['versionCommand'],
-        # https://github.com/gjtorikian/jekyll-last-modified-at/blob/master/lib/jekyll-last-modified-at/determinator.rb
-        lastModified: product.data['last_modified_at'].last_modified_at_time.iso8601,
-        auto: product.data.has_key?('auto'),
         cycles: product.data['releases']
-        .map { |cycle| {
-          name: cycle['releaseCycle'],
-          codename: cycle['codename'],
-          label: ApiV1.strip_html(cycle['label']),
-          date: cycle['releaseDate'],
-          support: cycle['support'],
-          lts: cycle['lts'],
-          eol: cycle['eol'],
-          discontinued: cycle['discontinued'],
-          extendedSupport: cycle['extendedSupport'],
-          latest: {
-            name: cycle['latest'],
-            date: cycle['latestReleaseDate'],
-            link: cycle['link'],
+          .map { |cycle| {
+            name: cycle['releaseCycle'],
+            codename: cycle['codename'],
+            label: ApiV1.strip_html(cycle['label']),
+            date: cycle['releaseDate'],
+            support: cycle['support'],
+            lts: cycle['lts'],
+            eol: cycle['eol'],
+            discontinued: cycle['discontinued'],
+            extendedSupport: cycle['extendedSupport'],
+            latest: {
+              name: cycle['latest'],
+              date: cycle['latestReleaseDate'],
+              link: cycle['link'],
+            }
           }
-        }}
+        }
+      }, {
+        # https://github.com/gjtorikian/jekyll-last-modified-at/blob/master/lib/jekyll-last-modified-at/determinator.rb
+        last_modified: product.data['last_modified_at'].last_modified_at_time.iso8601,
+        auto: product.data.has_key?('auto'),
       })
     end
   end
